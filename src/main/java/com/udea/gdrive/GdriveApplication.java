50cc401367f391a0f876187b16b5b34e8920a817
santycano <santy.cano@hotmail.com>
2021-09-14 10:34:22 -0500

    Initial commit

---
+++ src/main/java/com/udea/gdrive/GdriveApplication.java
@@ -0,0 +1,13 @@
+package com.udea.gdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udea/gdrive/GdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.gdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
